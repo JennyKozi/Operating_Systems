8002d19a16d0cbdfad55f290199f31366365db31
Evgenia Kozi <79666746+JennyKozi@users.noreply.github.com>
2023-11-08 01:08:24 +0200

    Add files via upload

--- PROJECT_02/mysort.c
+++ PROJECT_02/mysort.c
@@ -22,36 +22,36 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
-	int i, k, numOfrecords, rp, status, size;
+	int i, j, k, total_records, rp, stat, size, offset = 0;
 	long lSize;
 	char *data_file, *sort1, *sort2;
 	Record rec;
 	struct stat buffer;
 
-	int load_splitters = numOfrecords / k;
-	int remaining_load = numOfrecords % k; // The first 'remaining_load' (e.g. 5) children / spliters will get one extra record
-//	int total_sorters = (k * (k + 1)) / 2;
-
 	// Check that the flags are correct
 	for (i = 1; i <= 7; i += 2) {
+
 		// File with data
 		if (strcmp("-i", argv[i]) == 0) {
 			data_file = argv[i + 1];
 		}
+		
 		// k children
 		else if (strcmp("-k", argv[i]) == 0) {
 			if (Check_Int(argv[i + 1]) == 1) {
 				k = atoi(argv[i + 1]);
 			}
 			else {
-				perror("k must an integer!\n");
+				perror("k must a positive integer!\n");
 				exit(1);
 			}
 		}
+
 		// Sorting program 1
 		else if (strcmp("-e1", argv[i]) == 0) {
 			sort1 = argv[i + 1];
 		}
+
 		// Sorting program 2
 		else if (strcmp("-e2", argv[i]) == 0) {
 			sort2 = argv[i + 1];
@@ -67,62 +67,139 @@ int main(int argc, char *argv[]) {
 		perror("Cannot open file!\n");
 		exit(1);
 	}
+
 	// Get number of records
-	status = fstat(rp, &buffer);
-	if (status != 0) {
-		printf("Error reading file!\n");
+	stat = fstat(rp, &buffer);
+	if (stat != 0) {
+		perror("Error reading file!\n");
 		close(rp);
 		exit(1);
 	}
 	size = buffer.st_size;
-	numOfrecords = (int)size / sizeof(rec);
+	total_records = (int)size / sizeof(rec);
+	lseek(rp, offset, SEEK_SET); // Set rp in the beginning of the file
 
-	// Array for the splitters' ids
+	int load_splitter = total_records / k; // Number of records for one splitter
+	int remaining_load_splitters = total_records % k; // The first children (spliters) will get one extra record
+
+	// Useful info for the splitters
 	pid_t *splitters;
+	int *splitters_rp, *splitters_numof_records;
 	CHECK_MALLOC_NULL(splitters = malloc(k * sizeof(pid_t)));
+	CHECK_MALLOC_NULL(splitters_rp = malloc(k * sizeof(int)));
+	CHECK_MALLOC_NULL(splitters_numof_records = malloc(k * sizeof(int)));
+
+	// Number of records for the splitters
+	for (i = 0; i < k; i++) {
+		if (remaining_load_splitters == 0) {
+			splitters_numof_records[i] = load_splitter;
+		}
+		else {
+			if (i < remaining_load_splitters)
+				splitters_numof_records[i] = load_splitter + 1;
+			else
+				splitters_numof_records[i] = load_splitter;
+		}
+	}
+
+	// File pointers for each splitter
+	for (i = 0; i < k; i++) {
+		splitters_rp[i] = dup(rp); // Create a copy of the file pointer of the data file
+		lseek(splitters_rp[i], i * splitters_numof_records[i] * sizeof(rec), SEEK_SET); // Set the file pointer of this splitter to the right byte
+	}
 
 	// Create k splitters with fork
 	for (i = 0; i < k; i++) {
+
 		splitters[i] = fork();
 		if (splitters[i] == -1) {
 			perror("Failed to fork!\n");
 			exit(1);
 		}
+
 		// SPLITTERS
 		if (splitters[i] == 0) {
-			// Number of records for this splitter
-			int num_records_splitter;
-
 
 			// Number of sorters this splitter has
-			int num_sorters = k - i;
+			int numof_sorters = k - i;
+			int load_sorter = splitters_numof_records[i] / k; // The first children (spliters) will get one extra record
+			int remaining_load_sorters = splitters_numof_records[i] % k; // The first children (spliters) will get one extra record
 
-			// Create the sorters of this splitter
+			// Useful info for the sorters of this splitter
 			pid_t *sorters;
-			CHECK_MALLOC_NULL(sorters = malloc(num_sorters * sizeof(pid_t)));
+			int *sorters_rp, *sorters_numof_records;
+			CHECK_MALLOC_NULL(sorters = malloc(numof_sorters * sizeof(pid_t)));
+			CHECK_MALLOC_NULL(sorters_rp = malloc(numof_sorters * sizeof(int)));
+			CHECK_MALLOC_NULL(sorters_numof_records = malloc(numof_sorters * sizeof(int)));
 
-			// Create sorters with fork
-			for (i = 0; i < num_sorters; i++) {
-				sorters[i] = fork();
-				if (sorters[i] == -1) {
+			// Number of records for the sorters
+			for (j = 0; j < numof_sorters; j++) {
+				if (remaining_load_sorters == 0) {
+					sorters_numof_records[j] = load_sorter;
+				}
+				else {
+					if (j < remaining_load_sorters)
+						sorters_numof_records[j] = load_sorter + 1;
+					else
+						sorters_numof_records[j] = load_sorter;
+				}
+			}
+
+			// File pointers for each sorter
+			for (j = 0; j < numof_sorters; j++) {
+				sorters_rp[j] = dup(rp); // Create a copy of the file pointer of the data file
+				lseek(sorters_rp[j], j * sorters_numof_records[j] * sizeof(rec), SEEK_SET); // Set the file pointer of this splitter to the right byte
+			}
+
+			// Create sorters of this splitter with fork
+			for (j = 0; j < numof_sorters; j++) {
+
+				sorters[j] = fork();
+				if (sorters[j] == -1) {
 					perror("Failed to fork!\n");
 					exit(1);
 				}
+
 				// SORTERS
-				if (sorters[i] == 0) {
-					// Number of records for this sorter
-					int num_records_sorter;
+				if (sorters[j] == 0) {
 
+					char str1[10], str2[10];
+					snprintf(str1, sizeof(str1), "%d", sorters_rp[j]);
+					snprintf(str2, sizeof(str2), "%d", sorters_numof_records[j]);
 
-					//break;
+					// Execute a sorting algorithm
+					if (j % 2 == 0) {
+						execl("./quicksort", "./quicksort", str1, str2, (char *)NULL);
+						perror("exec failure!\n");
+						exit(1);
+					}
+					else {
+						execl("./bubblesort", "./bubblesort", str1, str2, (char *)NULL);
+						perror("exec failure!\n");
+						exit(1);
+					}
+				}
+
+				// Parent process: SPLITTER
+				else {
+
 				}
 			}
 			free(sorters);
+			free(sorters_rp);
+			free(sorters_numof_records);
+		}
+
+		// Parent process: MYSORT
+		else {
+
 		}
 	}
 
 	close(rp);
 	free(splitters);
+	free(splitters_rp);
+	free(splitters_numof_records);
 
 	return 0;
 }
@@ -130,13 +207,13 @@ int main(int argc, char *argv[]) {
 // Check that it is an int
 static int Check_Int(char *string) {
 
-	int i;
 	// Check for chars
-	for (i = 0; i < strlen(string); i++) {
+	for (int i = 0; i < strlen(string); i++) {
 		if (isalpha(string[i])) {
 			return 0;
 		}
 	}
+
 	// Check if num is float
 	float float_num = atof(string);
 	int num = float_num;
--- PROJECT_02/quicksort.c
+++ PROJECT_02/quicksort.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include "header.h"
 
-int main (int argc, char *argv[]) { // argv[1] = fp
+int main (int argc, char *argv[]) {
 
 	// Check number of arguments
 	if (argc != 3) {
-		perror("Wrong number of arguments!\n");
+		perror("Wrong number of arguments for quicksort!\n");
 		exit(1);
 	}
 
+	// File pointer
+	int rp = atoi(argv[1]);
+
 	// Number of records that will be sorted
 	int num_records = atoi(argv[2]);
-	
+
+	// Read the file and sort the records
+
 
 	return 0;
 }
